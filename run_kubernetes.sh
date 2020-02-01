2d1cabd3b0ea1396cecbe1b4ee0b076bca15f87c
root <root@ip-172-31-45-120.us-east-2.compute.internal>
2020-02-01 18:19:20 +0000

    upload docker sh updated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath='muzammilkazmi86/microservices:latest'
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservice --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/microservice 8000:80
 
+# Get logs
+kubectl logs 'kubectl get pods -o=name'
