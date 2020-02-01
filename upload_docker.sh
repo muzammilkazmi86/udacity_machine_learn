58f0ba117a273bb48c3d1557fd08cb29713d075f
root <root@ip-172-31-45-120.us-east-2.compute.internal>
2020-02-01 18:04:59 +0000

    upload docker sh updated

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath="muzammilkazmi86/microservices"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username muzammilkazmi86
+docker tag microservices $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
